d32a6497987e25018c07a483fa40aed23e88d727
Theodore Sun(孙辽东) <sunliaodong@inspur.com>
2018-03-06 09:59:12 +0800

    增加自定义属性发布到rest

--- applicationdemo/src/main/java/com/sunld/controller/PublishController.java
+++ applicationdemo/src/main/java/com/sunld/controller/PublishController.java
@@ -2,6 +2,8 @@ package com.sunld.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,26 @@ import com.sunld.conf.CourseLearningMyConfigAuto;
 //@RestController的意思就是controller里面的方法都以json格式输出，不用再写什么jackjson配置的了！
 @RestController
 //这个属性也是必须的
-//@EnableConfigurationProperties({CourseLearningMyConfigAuto.class})
+@EnableConfigurationProperties({CourseLearningMyConfigAuto.class})
+@PropertySource(value = 
+						{
+								"classpath:/application.properties", 
+								"classpath:/myconfig.properties" //如果是相同的key，则最后一个起作用   
+						},
+						encoding = "UTF-8"
+)
 public class PublishController {
 	
 	@Value(value = "${server.port}")
 	private String port;
+	
+	@Value("${com.sunld.applicationp.chapter}")
+	private String chapter;
+	
+	@Value("${spring.http.encoding.force}")
+	private String springforce;
+	
+	
 	
 	@Autowired
 	private CourseLearningMyConfigAuto courseLearningMyConfigAuto;
@@ -22,7 +39,9 @@ public class PublishController {
 	@RequestMapping("/publishmyconfig")
     public String publishMyConfig() {
 		
-		System.out.println(port+"===========");
+		System.out.println(port+"====port=======");
+		System.out.println(chapter+"==chapter=========");
+		System.out.println(springforce+"==springforce=========");
 		
 		System.out.println(courseLearningMyConfigAuto + "=========");
 		System.out.println("章节：" + courseLearningMyConfigAuto.getChapter());
--- applicationdemo/src/test/java/com/sunld/PublishControllerTest.java
+++ applicationdemo/src/test/java/com/sunld/PublishControllerTest.java
@@ -3,8 +3,9 @@ package com.sunld;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,11 +14,15 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.sunld.conf.CourseLearningMyConfigAuto;
 import com.sunld.controller.PublishController;
 
 @RunWith(SpringRunner.class)
+//@ComponentScan("com.sunld")
+//@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class)
+//@SpringBootTest(classes=WebAppConfiguration.class)// 指定spring-boot的启动类    
 @SpringBootTest
-@ComponentScan("com.sunld")
+//@SpringApplicationConfiguration(classes = WebAppConfig.class)// 1.4.0 前版本  注意启动类不要搞错了 
 /**
  * 
  * @author sunliaodong
@@ -26,11 +31,26 @@ import com.sunld.controller.PublishController;
  * 这样我们创建的HelloController就可以在@Before函数中创建并传递到MockMvcBuilders.standaloneSetup（）函数中。
  */
 public class PublishControllerTest {
+	
+	@Value(value = "${server.port}")
+	private String port;
+	@Value("${com.sunld.applicationp.chapter}")
+	private String chapter;
+	
+	@Value("${spring.http.encoding.force}")
+	private String springforce;
+	
 	private MockMvc mvc;
 	
     @Before
     public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(new PublishController()).build();
+    	
+    	System.out.println("获取配置文件中的port：" + port);
+    	System.out.println("获取配置文件中的chapter：" + chapter);
+    	System.out.println("获取配置文件中的springforce：" + springforce);
+    	//使用new的方式会导致参数获取不到
+        //mvc = MockMvcBuilders.standaloneSetup(new PublishController()).build();
+    	mvc = MockMvcBuilders.standaloneSetup(publishController).build();
     }
     
     @Test
@@ -41,4 +61,26 @@ public class PublishControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andReturn();
     }
+    
+    @Autowired
+	private CourseLearningMyConfigAuto courseLearningMyConfigAuto;
+	
+    @Test
+    public void testGetConfig() {
+		
+		System.out.println(courseLearningMyConfigAuto + "=========");
+		System.out.println("章节：" + courseLearningMyConfigAuto.getChapter());
+		System.out.println("课程：" + courseLearningMyConfigAuto.getCourse());
+		System.out.println("学习：" + courseLearningMyConfigAuto.getLearning());
+    }
+    
+    @Autowired
+	private PublishController publishController;
+	
+    @Test
+    public void testPublishMyConfigByController() {
+    	publishController.publishMyConfig();
+    }
+    
+    
 }
