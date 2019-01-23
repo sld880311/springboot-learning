119378b7e179d9b976e10aa1022850dffe8dabe7
Theodore Sun(孙辽东) <sld880311@126.com>
2019-01-23 17:06:08 +0800

    初始化eureka-server代码

---
+++ springboot-eureka-server/src/test/java/com/sunld/eureka/server/SpringbootEurekaServer10000ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sunld.eureka.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootEurekaServer10000ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
